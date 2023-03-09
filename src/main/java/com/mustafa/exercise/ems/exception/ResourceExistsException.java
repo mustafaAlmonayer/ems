5df5af366500ba10b95498936095e9d86c8fbf5c
mustafaAlmonayer <mustafakamal123435@gmail.com>
2023-03-09 18:10:40 +0300

    iodjwq

--- src/main/java/com/mustafa/exercise/ems/exception/ResourceExistsException.java
+++ src/main/java/com/mustafa/exercise/ems/exception/ResourceExistsException.java
@@ -1,11 +1,11 @@
 package com.mustafa.exercise.ems.exception;
 
 public class ResourceExistsException extends RuntimeException {
-	
+
 	private static final long serialVersionUID = -3633650953857247774L;
 
 	public ResourceExistsException(String message) {
 		super(message);
 	}
-	
+
 }
